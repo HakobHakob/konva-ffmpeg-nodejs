8752e7d1c876108dba188d47796a804a684229cd
Hakob <45873619+HakobHakob@users.noreply.github.com>
2023-06-03 11:25:03 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-//node src/index.js
 require("konva-node")
 const { renderVideo } = require("./renderVideo")
 const path = require("path")
-// const uuid = require("uuid")
-
 
 const run = async () => {
   const outputDir = path.join(__dirname,"../tmpVideo")
   const output = path.join(__dirname,`../../tmpVideo/output-video-1.mp4`)
-
   await renderVideo({outputDir,output})
 }
 
